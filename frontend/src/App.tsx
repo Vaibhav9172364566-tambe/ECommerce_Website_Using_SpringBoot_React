f3bfdd30d6c8a0041ac92f015e7208a030426c42
Vaibhav Tambe <tambevaibhav207@gmail.com>
2024-10-28 11:01:13 +0530

    Install react and tailwind-css/MUI/Icons

---
+++ frontend/src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Button } from '@mui/material';
+import HomeIcon from '@mui/icons-material/Home';
+
+
+function App() {
+  return (
+   <>
+   <div className='p-20'> 
+    
+   </div>
+   </>
+  );
+}
+
+export default App;
