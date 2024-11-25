e8f9a290e22e3babd4f344d780bf7b1f5ed314ca
GayatriAWankhade <gayatri01wankhade@gmail.com>
2024-11-25 12:15:58 +0530

    Order item is complated

--- frontend/src/customer/pages/Account/Account.tsx
+++ frontend/src/customer/pages/Account/Account.tsx
@@ -1,6 +1,7 @@
 import { Divider } from '@mui/material'
 import React from 'react'
 import { useLocation, useNavigate } from 'react-router-dom'
+import Orders from './Orders'
 
 const menu=[
     { name:"orders",path:'/account/orders'  },
@@ -43,8 +44,8 @@ const Account = () => {
                     }
             </section>
 
-            <section className='right'>
-                    Right
+            <section className='right lg:col-span-2 lg:pl-2 py-5'>
+                    <Orders></Orders>
             </section>
 
          </div>
--- frontend/src/customer/pages/Account/OrderItem.tsx
+++ frontend/src/customer/pages/Account/OrderItem.tsx
@@ -1,8 +1,42 @@
+import { ElectricBolt } from '@mui/icons-material'
+import { Avatar } from '@mui/material'
+import { teal } from '@mui/material/colors'
 import React from 'react'
 
 const OrderItem = () => {
   return (
-    <div>OrderItem</div>
+    <div className='text-sm bg:white p-5 space-y-4 border rounded-md cursor-pointer'>
+        <div className='flex items-center gap-5'>
+            <div>
+                <Avatar sizes='small' sx={{bgcolor:teal[500]}}><ElectricBolt></ElectricBolt></Avatar>
+
+            </div>
+            <div>
+                <h1 className='font-bold text-primary'>PENDING</h1>
+                <p>Arriving By MOn,15 Jul</p>
+
+            </div>
+
+        </div>
+
+        <div className='p-5 bg-teal-50 flex gap-3'> 
+            <div>
+                <img 
+                className='w-[70px]'
+                src='https://media.gettyimages.com/id/172380146/photo/gold-watch.jpg?s=2048x2048&w=gi&k=20&c=HjkjL79rKuEFWYQRTVUHO8SLyyn7-v_y1dYQmDZTeXU=' alt=''></img>
+            </div>
+            <div className='w-full space-y-2'>
+                <h1 className='font-bold'>Virani Clothing  </h1>
+                <p>Cellecor RAY 1.43" AMOLED Dispaly | 700 NITS | AOD | BT-Calling | Ai VOICE | Split screen Samrtwath (black Strap,Free Size)</p>
+
+                <p>
+                    <strong>size : </strong>FREE
+                </p>
+            </div>
+
+        </div>
+
+    </div>
   )
 }
 
