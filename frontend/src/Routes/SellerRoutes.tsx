a4a1effafbc8071bf18979c1803fef24e06b747c
GayatriAWankhade <gayatri01wankhade@gmail.com>
2024-11-30 16:29:12 +0530

    impliment the structre of seller Payment

--- frontend/src/Routes/SellerRoutes.tsx
+++ frontend/src/Routes/SellerRoutes.tsx
@@ -1,28 +1,27 @@
-import React from 'react'
-import { Routes,Route } from 'react-router-dom'
-
-import DashBoard from '../seller/pages/SellerdashBoard/DashBoard'
-import Products from '../seller/pages/Products/Products'
-import Orders from '../seller/pages/Order/Orders'
-import Profile from '../seller/pages/Account/Profile'
-import Transactions from '../seller/pages/Payment/Transactions'
-import AddProducts from '../seller/pages/Products/AddProducts'
-import Payments from '../seller/pages/Payment/Payments'
+import { Route, Routes } from 'react-router-dom';
+import Profile from '../seller/pages/Account/Profile';
+import Orders from '../seller/pages/Order/Orders';
+import Payments from '../seller/pages/Payment/Payments';
+import AddProducts from '../seller/pages/Products/AddProducts';
+import Products from '../seller/pages/Products/Products';
+import DashBoard from '../seller/pages/SellerdashBoard/DashBoard';
+import Transaction from '../seller/pages/Payment/Transaction';
 
 const SellerRoutes = () => {
   return (
     <div>
-        <Routes>
-              <Route path='/' element={<DashBoard></DashBoard>}></Route>
-              <Route path='/products' element={<Products ></Products>}></Route>
-              <Route path='/add-product' element={<AddProducts></AddProducts>}></Route>
-              <Route path='/orders' element={<Orders></Orders>}></Route>
-             <Route path='/account' element={<Profile></Profile>}></Route>
-             <Route path='/payment' element={<Payments></Payments>}></Route>
-             <Route path='/transaction' element={<Transactions></Transactions>}></Route>
-          </Routes>
+      <Routes>
+        <Route path="/" element={<DashBoard />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/add-product" element={<AddProducts />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/account" element={<Profile />} />
+        <Route path="/payment" element={<Payments />} />
+        <Route path='/transaction' element={<Transaction></Transaction>}></Route>
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default SellerRoutes
+export default SellerRoutes;
+
--- frontend/src/seller/pages/Payment/Payments.tsx
+++ frontend/src/seller/pages/Payment/Payments.tsx
@@ -1,8 +1,26 @@
+import { Button, Card, Divider } from '@mui/material'
 import React from 'react'
+import Transaction from './Transaction'
 
 const Payments = () => {
   return (
-    <div>Paymenst</div>
+    <div className='space-x-5'>
+      <Card className='rounded-md space-y-4 p-5'>
+        <h1 className='text-gray-600 font-medium'>Total Earing</ h1>
+        <h1 className='font-bold text-xl pb-1'>₹11644</h1>
+        <Divider></Divider>
+        <p>Last Payment : <strong>₹0</strong></p>
+
+      </Card>
+      <div className='pt-20 space-y-3 '>
+      <Button variant='contained'>
+        Transaction
+      </Button>
+      <Transaction></Transaction>
+      </div>
+
+      
+    </div>
   )
 }
 
